e6fc52ce106172d2f39fee26a4f5686d350c20e2
Rabia790 <rabiaangel9921@gmail.com>
2022-12-05 19:20:45 -0500

    Implemented Spinner

--- app/src/main/java/com/example/rabia_880823_mt/MainActivity.java
+++ app/src/main/java/com/example/rabia_880823_mt/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.rabia_880823_mt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.cars);
+        //spinner.setOnItemSelectedListener(this);
+
+        Spinner spinner = (Spinner) findViewById(R.id.cars);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.cars, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
